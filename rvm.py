7f2917460ca919e63c2e2bee3a517582ff9932ba
Tom Bolton <thomasmichaelbolton@gmail.com>
2019-02-28 14:12:31 +0000

    bugs in rvm.py

--- rvm.py
+++ rvm.py
@@ -76,7 +76,7 @@ class BaseRVM(BaseEstimator):
             'beta': self.beta,
             'beta_fixed': self.beta_fixed,
             'bias_used': self.bias_used,
-            'verbose': self.verbose
+            'verbose': self.verbose,
             'verbose_frequency': self.verb_freq
         }
         return params
@@ -104,7 +104,6 @@ class BaseRVM(BaseEstimator):
             if self.bias_used:
                 keep_alpha[-1] = True
 
-        self.relevance_ = self.relevance_[ keep_alpha ]
         self.labels = self.labels[ keep_alpha ]
 
 		# prune all parameters to new set of basis functions
@@ -140,14 +139,13 @@ class BaseRVM(BaseEstimator):
 			self.phi[:,:n_features] = X
 			self.phi[:,-1] = 1           # bias
 			
-		else :
+        else :
 			self.phi = X
         
         self.labels = np.array( X_labels )
 
         n_basis_functions = self.phi.shape[1]
 
-        self.relevance_ = X
         self.y = y
 
         self.alpha_ = self.alpha * np.ones(n_basis_functions)
@@ -156,6 +154,11 @@ class BaseRVM(BaseEstimator):
         self.m_ = np.zeros(n_basis_functions)
 
         self.alpha_old = self.alpha_
+        
+        if self.verbose :
+            print "Fit: beginning with {} candidate terms".format( n_basis_functions )
+            print "Fit: {}\n".format( X_labels )
+            
 
         for i in range(self.n_iter):
 			
@@ -177,14 +180,15 @@ class BaseRVM(BaseEstimator):
                 print "--Beta {}".format( self.beta_ ) 
                 print "--Gamma {}".format( self.gamma ) 
                 print "--m {}".format( self.m_ ) 
-                print "--# of relevance vectors {} / {}".format( self.relevance_.shape[0], n_basis_functions ) 
+                print "--# of relevance vectors {} / {}".format( self.phi.shape[1], n_basis_functions ) 
                 print "--Remaining candidate terms {}\n".format( list( self.labels ) )
 
 			# check if threshold has been reached
             delta = np.amax( np.absolute( self.alpha_ - self.alpha_old ) )
 
-            if delta < self.tol and i > 1:
+            if delta < self.tol and i > 1 :
                 print "Fit: delta < tol @ iteration {}, finished.".format(i)
+                print "Fit: weights {}".format(self.m_)
                 break
 
             self.alpha_old = self.alpha_
